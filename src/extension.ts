ce6c7de7d84f7d5004a79bbb784764e2bf5ecc99
Eirik Årdal <eirik.ardal@bulderbank.no>
2021-07-02 12:42:53 +0200

    Clean up commented code

--- src/extension.ts
+++ src/extension.ts
@@ -8,22 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(disposePeekImports);
-
-  // vscode.workspace.onDidCloseTextDocument((doc) => {
-  //   console.log(`onDidCloseTextDocument: ${doc}`);
-  // });
-  // vscode.window.onDidChangeActiveTextEditor((args) => {
-  //   console.log(`onDidChangeActiveTextEditor: ${args}`);
-  // });
-  // vscode.window.onDidChangeTextEditorSelection((args) => {
-  //   console.log(`onDidChangeTextEditorSelection: ${args}`);
-  // });
-  // vscode.window.onDidChangeVisibleTextEditors((args) => {
-  //   console.log(`onDidChangeTextEditorSelection: ${args}`);
-  // });
-  // vscode.window.onDidChangeWindowState((args) => {
-  //   console.log(`onDidChangeWindowState: ${args}`);
-  // });
 }
 
 export function deactivate() {}
